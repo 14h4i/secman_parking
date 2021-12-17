e7af0321bf3075925301197d3c01c2d6d0e4da0a
14h4i <pgkhai.dev@gmail.com>
2021-12-17 11:07:32 +0700

    Update model

--- lib/models/card.dart
+++ lib/models/card.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:secman_parking/utils/date_time_intl.dart';
 
 part 'card.g.dart';
 
@@ -14,30 +15,67 @@ class Card {
   @JsonKey(name: 'is_guest')
   final bool? isGuest;
 
-  @JsonKey(name: 'time_in', fromJson: _fromJson, toJson: _toJson)
+  @JsonKey(
+      name: 'time_in',
+      fromJson: _datetimeFromTimestamp,
+      toJson: _datetimeToTimestamp)
   final DateTime? timeIn;
 
-  @JsonKey(name: 'time_out', fromJson: _fromJson, toJson: _toJson)
+  @JsonKey(
+      name: 'time_out',
+      fromJson: _datetimeFromTimestamp,
+      toJson: _datetimeToTimestamp)
   final DateTime? timeOut;
 
   @JsonKey(name: 'vehicle_number')
   final String? vehicleNumber;
 
-  Card(
+  @JsonKey(name: 'records', fromJson: _mapFromJson, toJson: _jsonToMap)
+  final Map<DateTime, String>? records;
+
+  Card({
     this.id,
     this.currentPhoto,
     this.isGuest,
     this.timeIn,
     this.timeOut,
     this.vehicleNumber,
-  );
+    this.records,
+  });
 
   factory Card.fromJson(Map<String, dynamic> json) => _$CardFromJson(json);
 
   Map<String, dynamic> toJson() => _$CardToJson(this);
 
-  static DateTime? _fromJson(Timestamp? timestamp) => timestamp?.toDate();
+  static DateTime? _datetimeFromTimestamp(Timestamp? timestamp) =>
+      timestamp?.toDate();
 
-  static Timestamp? _toJson(DateTime? time) =>
+  static Timestamp? _datetimeToTimestamp(DateTime? time) =>
       time != null ? Timestamp.fromDate(time) : null;
+
+  static Map<DateTime, String>? _mapFromJson(Map<String, dynamic>? map) =>
+      map?.map(
+          (key, value) => MapEntry(DateTimeIntl.stringToDateTime(key), value));
+
+  static Map<String, dynamic>? _jsonToMap(Map<DateTime, String>? map) =>
+      map?.map(
+          (key, value) => MapEntry(DateTimeIntl.dateTimeToString(key), value));
+
+  Card? copyWith({
+    String? id,
+    String? currentPhoto,
+    bool? isGuest,
+    DateTime? timeIn,
+    DateTime? timeOut,
+    String? vehicleNumber,
+  }) {
+    return Card(
+      id: id ?? this.id,
+      currentPhoto: currentPhoto ?? this.currentPhoto,
+      isGuest: isGuest ?? this.isGuest,
+      timeIn: timeIn ?? this.timeIn,
+      timeOut: timeOut ?? this.timeOut,
+      vehicleNumber: vehicleNumber ?? this.vehicleNumber,
+    );
+  }
 }
--- lib/models/card.g.dart
+++ lib/models/card.g.dart
@@ -7,19 +7,21 @@ part of 'card.dart';
 // **************************************************************************
 
 Card _$CardFromJson(Map<String, dynamic> json) => Card(
-      json['id'] as String?,
-      json['current_photo'] as String?,
-      json['is_guest'] as bool?,
-      Card._fromJson(json['time_in'] as Timestamp?),
-      Card._fromJson(json['time_out'] as Timestamp?),
-      json['vehicle_number'] as String?,
+      id: json['id'] as String?,
+      currentPhoto: json['current_photo'] as String?,
+      isGuest: json['is_guest'] as bool?,
+      timeIn: Card._datetimeFromTimestamp(json['time_in'] as Timestamp?),
+      timeOut: Card._datetimeFromTimestamp(json['time_out'] as Timestamp?),
+      vehicleNumber: json['vehicle_number'] as String?,
+      records: Card._mapFromJson(json['records'] as Map<String, dynamic>?),
     );
 
 Map<String, dynamic> _$CardToJson(Card instance) => <String, dynamic>{
       'id': instance.id,
       'current_photo': instance.currentPhoto,
       'is_guest': instance.isGuest,
-      'time_in': Card._toJson(instance.timeIn),
-      'time_out': Card._toJson(instance.timeOut),
+      'time_in': Card._datetimeToTimestamp(instance.timeIn),
+      'time_out': Card._datetimeToTimestamp(instance.timeOut),
       'vehicle_number': instance.vehicleNumber,
+      'records': Card._jsonToMap(instance.records),
     };
