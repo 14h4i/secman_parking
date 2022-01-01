53ab95d9a4775f94af484b6370b95e0784a6c4fb
14h4i <pgkhai.dev@gmail.com>
2022-01-01 17:49:37 +0700

    update ui

--- lib/utils/ui_data.dart
+++ lib/utils/ui_data.dart
@@ -1,4 +1,5 @@
 class UIData {
   static const String _imageDir = "assets/images";
   static const String scanNfc = "$_imageDir/scan_nfc.png";
+  static const String logoSecman = "$_imageDir/secman.png";
 }
