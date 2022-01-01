4e6460cc9f90ada94320128558e672ca6f430125
14h4i <pgkhai.dev@gmail.com>
2022-01-01 17:50:49 +0700

    welcome layout

--- lib/modules/authentication/pages/welcome_page.dart
+++ lib/modules/authentication/pages/welcome_page.dart
@@ -1,30 +1,44 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:secman_parking/blocs/app_bloc.dart';
+import 'package:secman_parking/route/route_name.dart';
+import 'package:secman_parking/themes/app_text_style.dart';
 
-class WelcomePage extends StatefulWidget {
+class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
-
-  @override
-  State<WelcomePage> createState() => _WelcomePageState();
-}
-
-class _WelcomePageState extends State<WelcomePage> {
-  AppBloc? get appStateBloc => BlocProvider.of<AppBloc>(context);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Login'),
-          onPressed: _changeAppState,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            AutoSizeText(
+              'WELCOME',
+              maxLines: 1,
+              style: AppTextStyle.largeTitleCard.copyWith(color: Colors.blue),
+            ),
+            ElevatedButton(
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: AutoSizeText(
+                  'Bắt đầu',
+                  maxLines: 1,
+                  style: AppTextStyle.h3,
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, RouteName.loginPage);
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 
-  void _changeAppState() {
-    appStateBloc!.add(ChangeAppStateEvent(state: const AuthorizedAppState()));
-  }
+  // void _changeAppState() {
+  //   appStateBloc!.add(ChangeAppStateEvent(state: const AuthorizedAppState()));
+  // }
 }
