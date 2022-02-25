24826bc21841c13603e505fb553a01b1cc346e2e
14h4i <pgkhai.dev@gmail.com>
2022-02-25 12:16:15 +0700

    update

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -6,7 +6,6 @@ import 'package:secman_parking/modules/authentication/pages/pin_code_page.dart';
 import 'package:secman_parking/modules/authentication/pages/welcome_page.dart';
 import 'package:secman_parking/modules/fee/blocs/fee_bloc.dart';
 import 'package:secman_parking/modules/fee/pages/fee_page.dart';
-import 'package:secman_parking/modules/manager/blocs/manager_bloc.dart';
 import 'package:secman_parking/modules/manager/pages/add_internal_card_page.dart';
 import 'package:secman_parking/modules/dashboard/pages/dashboard_page.dart';
 import 'package:secman_parking/modules/manager/pages/manager_page.dart';
